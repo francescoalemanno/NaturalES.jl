0029bfaf360e5e32e4aba943d866e1445d1d61e6
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-04-11 14:18:09 +0200

    further improvements

--- src/NaturalES.jl
+++ src/NaturalES.jl
@@ -1,27 +1,37 @@
 module NaturalES
 using Random
-function optimize(f,x0,σ,lr)
+function optimize(f,x0::AbstractArray{T},σ,lr) where T
     N=length(x0)
     ϵ=copy(x0)
     ∇f=copy(x0)
     x=copy(x0)
-    Z=zero(f(x0))
-    samples=4N
-    #bestavgF=
-    for steps in 1:1000
+    Z=zero(T)
+    samples=3N
+    MAXWASTEEVAL=500
+    improv=MAXWASTEEVAL
+    bestavgF=typemax(T)
+    iσ=inv.(σ)
+    while improv > 0
         avgF=Z
         for i in 1:samples
             randn!(ϵ)
-            F=f(x .+ σ.*ϵ)
-            avgF+=F
-            ∇f.+=F.*ϵ./σ
+            Fa = f(x .+ σ .* ϵ)
+            Fb = f(x .- σ .* ϵ)
+            avgF += (Fa + Fb) / 2
+            δ = (Fa - Fb) / 2
+            ∇f .+= δ .* ϵ .* iσ
         end
-        avgF/=samples
-        ∇f./=samples
-        x.-=lr.*∇f
-        ∇f.=0
-        @show avgF
+        avgF /= samples
+        if avgF < bestavgF
+            improv = MAXWASTEEVAL
+            bestavgF = avgF
+        else
+            improv -= 1
+        end
+        ∇f ./= samples
+        x .-= lr .* ∇f
+        ∇f .= 0
     end
-    x
+    (sol=x,cost=f(x))
 end
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,8 @@
 using NaturalES
 using Test
+f(x)=sum(cosh.(x))-length(x)
+@testset "Simple Functions" begin
+    @test optimize(f,[1.0,0.5,2.0],0.1,0.1)[2] < 1e5
+    @test optimize(f,[1.0,0.5],0.1,0.1)[2] < 1e5
+    @test optimize(f,[1.0 0.5; 0.5 1.0],0.1,0.1)[2] < 1e5
+end
