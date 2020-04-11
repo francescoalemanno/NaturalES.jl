013ee29d8f1986f1c6134589d6c1ceb9290d5c04
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-04-11 21:51:11 +0200

    better test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using NaturalES
 using Test
 
-f(x) = sum(cosh.(x)) - length(x)
+f(x) = sum(cosh.(x.-(1:length(x)))) - length(x)
 g(p) = rand()<p[1] ? 1 : 2
+
 @testset "Simple Functions" begin
     @test optimize(f,(1.0,0.5,2.0),0.1,0.1)[2] < 1e-5
     @test optimize(f,(1.0,0.5),0.1,0.1)[2] < 1e-5
