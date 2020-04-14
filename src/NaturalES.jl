8a3633ab8ec8a5aeb1373904fa331916e2dafe67
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-04-14 21:09:18 +0200

    enanchments to sNES

--- src/NaturalES.jl
+++ src/NaturalES.jl
@@ -2,75 +2,59 @@ module NaturalES
 using Random
 using LinearAlgebra
 
-trandn(rn::AbstractRNG, v::NTuple{N,T}) where {N,T} = ntuple(i->randn(rn,T),Val(N))
-function tsqnorm(v::NTuple{N,T}) where {N,T}
-    s=zero(T)
-    @fastmath for i in eachindex(v)
-        e=@inbounds v[i]
-        s+=e*e
-    end
-    s
-end
 
-function separable_nes(f,x0::NTuple{N,T},σ::NTuple{N,T},lr_x::T,lr_σ::T) where {T,N}
+function separable_nes(f,x0::AbstractVector{T},σ::AbstractVector{T},lr_x::T,lr_σ::T) where T
+    N=length(x0)
+    length(σ)==N || error("the length of 'σ' must be equal to the length of 'x'.")
     rn=MersenneTwister(123+3N)
-    x=x0
-    Zv=zero.(x0)
+    x=copy(x0)
     samples=4 + ceil(Int, log(3*N))
-    MAXWASTEEVAL=500*samples
-    improv=MAXWASTEEVAL
-    bestavgF=10f(x0)
-    Z=zero(bestavgF)
+    Z=zero(T)
     O=one(T)
     mirror=(-O,O)
     Nm=length(mirror)
-    while improv > 0
-        avgF=Z
-        ∇f=Zv
-        ∇fσ=Zv
+    F=fill(f(x0),samples)
+    ∇f=fill(Z,N)
+    ∇fσ=fill(Z,N)
+    ϵ=map(i->zeros(T,N),1:samples)
+    u=@. max(0,log(samples/2+1)-log(1:samples))
+    u=T.(u./sum(u) .- 1/samples)
+    idx=collect(1:samples)
+    tmp_x=copy(x0)
+    for i in 1:samples
+        randn!(rn,ϵ[i])
+        tmp_x .= x .+ σ .* ϵ[i]
+        F[i] = f(tmp_x)
+    end
+    sort!(idx,by=i->F[i])
+    while sum(σ) > 1e-5
         for i in 1:samples
-            ϵ=trandn(rn,x0)
-            for s in mirror
-                F = f(x .+ s .* σ .* ϵ)
-                avgF += F
-                ∇f = ∇f .+ s*F .* ϵ
-                ∇fσ = ∇fσ .+ F .* (ϵ.*ϵ .- O)
+            j=idx[i]
+            if i>=(samples÷2)
+                randn!(rn,ϵ[j])
             end
+            tmp_x .= x .+ σ .* ϵ[j]
+            F[j] = f(tmp_x)
         end
-        avgF /= Nm*samples
-        if avgF < bestavgF
-            improv = MAXWASTEEVAL
-            bestavgF = avgF
-        else
-            improv -= 1
+        sort!(idx,by=i->F[i])
+        ∇f.=Z
+        ∇fσ.=Z
+        for i in 1:samples
+            j=idx[i]
+            ∇f .+= u[i] .* ϵ[j]
+            ∇fσ .+= u[i] .* (ϵ[j] .* ϵ[j] .- O)
         end
-        g=(∇fσ...,∇f...)
-
-        if !all(isfinite,g)
-            throw(ErrorException("Function produced $avgF near $x :\n try lowering σ, the learning rate, or changing initial point."))
-        end
-
-        ∇f = clamp.(∇f ./ (Nm*samples),-O,O)
-        ∇fσ = clamp.(∇fσ ./ (Nm*samples),-O/lr_σ,O/lr_σ)
-        x = x .- lr_x .* σ .* ∇f
-        σ = σ .* exp.(-lr_σ ./ 2 .*∇fσ)
+        x .+= lr_x .* σ .* ∇f
+        σ .*= exp.(lr_σ ./ 2 .*∇fσ)
         #@show x,σ,avgF
     end
     (sol=x, cost=f(x))
 end
 
-function separable_nes(f,x0::NTuple{N,T},
-    σ::NTuple{N,T}=ntuple(i->one(T),Val(N)),
-    lr_x=one(T),
-    lr_σ=convert(T,(3 + log(N)) / (5 * sqrt(N)))
-    ) where {N,T}
-    separable_nes(f,x0,vσ,lr_x,lr_σ)
-end
-
 function xnes(f,μ::AbstractVector{T},A::AbstractMatrix{T}) where T
     d=length(μ)
     n=4+floor(Int,3*log(d))
-    Z=[Array{T}(undef,d) for i in 1:n]
+    Z=Array{T}(undef,d,n)
     σ=abs(det(A))^(1/d)
     F=fill(f(μ),n)
     B=A./σ
@@ -80,13 +64,13 @@ function xnes(f,μ::AbstractVector{T},A::AbstractMatrix{T}) where T
     ημ=one(T)
     ησ=ηB=3/5*(3+log(d))/(d*√d)
     while σ>1e-5
+        randn!(Z)
         for i in 1:n
-            randn!(Z[i])
-            F[i]=f(μ .+ σ.*B*Z[i])
+            F[i]=f(μ .+ σ.*B*Z[:,i])
         end
-        sort!(idx,by=i->F[i])
-        Gδ=sum(u[i].*Z[idx[i]] for i in 1:n)
-        GM=sum(u[i].*(Z[idx[i]]*(Z[idx[i]]')-I) for i in 1:n)
+        su=u[invperm(sort(idx,by=i->F[i]))]
+        Gδ=sum(su[i].*Z[:,i] for i in 1:n)
+        GM=sum(su[i].*(Z[:,i]*(Z[:,i]')-I) for i in 1:n)
         Gσ=tr(GM)/d
         GB=GM-Gσ*I
         μ=μ.+ημ*σ*B*Gδ
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,20 @@
 using NaturalES
 using Test
 using LinearAlgebra
+
 f(x) = sum(cosh.(x.-(1:length(x)))) - length(x)
+
 g(P) = sum(0<rand()<p<=1 ? 1 : 5+p^2 for p in P)
 
-@testset "Simple Functions" begin
-    @test separable_nes(f,(0.0,0.0,0.0)).cost < 1e-5
-    @test separable_nes(g,(5.0,5.0,5.0),(1.0,1.0,1.0),0.1,0.01).cost ==3
-    @test xnes(f,[0.0,0.0]).cost < 1e-5
+function rosenbrock2d(x)
+  return (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
 end
-using BenchmarkTools
 
-xi=zeros(10)
-@btime xnes(f,$xi) # 82.308 ms (187816 allocations: 46.74 MiB)
+@testset "Simple Functions" begin
+    @test separable_nes(f,[0.0,0.0,0.0],[1.0,1.0,1.0],0.1,0.1).cost < 1e-5
+    @test separable_nes(f,[4.0,4.0,4.0],[1.0,1.0,1.0],0.1,0.1).cost < 1e-5
+    @test separable_nes(rosenbrock2d,[4.0,4.0],[1.0,1.0],0.1,0.1).cost < 1e-5
+    @test separable_nes(g,[4.0,4.0,4.0],[1.0,1.0,1.0],0.1,0.1).cost ==3
+    @test xnes(f,[0.0,0.0]).cost < 1e-5
+    @test xnes(rosenbrock2d,[0.5,0.5]).cost < 1e-5
+end
