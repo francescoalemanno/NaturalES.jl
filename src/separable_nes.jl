c4324cf6b8c47142762959f84d2d7faf2b75eb42
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-04-16 16:00:45 +0200

    whitespaces

--- src/separable_nes.jl
+++ src/separable_nes.jl
@@ -19,53 +19,52 @@ end
 function separable_nes(f,x0::AbstractVector{T},σ::AbstractVector{T},params::sNES{T}) where T
     samples=params.samples
     samples>3 || error("atleast 3 samples are required for sNES to work.")
-    ημ=params.ημ
-    ησ=params.ησ
-    σtol=params.σtol
-
+    ημ = params.ημ
+    ησ = params.ησ
+    σtol = params.σtol
     N=length(x0)
-    length(σ)==N || error("the length of 'σ' must be equal to the length of 'x'.")
-    rn=MersenneTwister(123+3N)
-    x=copy(x0)
-    Z=zero(T)
-    O=one(T)
-    F=fill(f(x0),samples)
-    ∇f=fill(Z,N)
-    ∇fσ=fill(Z,N)
-    ϵ=map(i->zeros(T,N),1:samples)
-    u=utility_function{T}(samples)
-    idx=collect(1:samples)
-    tmp_x=copy(x0)
+    length(σ) == N || error("the length of 'σ' must be equal to the length of 'x'.")
+    rn = MersenneTwister(123+3N)
+    x = copy(x0)
+    Z = zero(T)
+    O = one(T)
+    F = fill(f(x0),samples)
+    ∇f = fill(Z,N)
+    ∇fσ = fill(Z,N)
+    ϵ = map(i->zeros(T,N), 1:samples)
+    u = utility_function{T}(samples)
+    idx = collect(1:samples)
+    tmp_x = copy(x0)
     for i in 1:samples
-        randn!(rn,ϵ[i])
+        randn!(rn, ϵ[i])
         tmp_x .= x .+ σ .* ϵ[i]
         F[i] = f(tmp_x)
     end
     sort!(idx,by=i->F[i])
     while geo_mean(σ) > σtol
         for i in 1:samples
-            j=idx[i]
-            if i>=(samples÷2)
-                randn!(rn,ϵ[j])
+            j = idx[i]
+            if i >= (samples ÷ 2)
+                randn!(rn, ϵ[j])
             else
-                rescaling=sqrt(randchisq(rn,T,N))/norm(ϵ[j])
-                ϵ[j].*=rescaling
+                rescaling = sqrt(randchisq(rn, T, N)) / norm(ϵ[j])
+                ϵ[j] .*= rescaling
             end
             tmp_x .= x .+ σ .* ϵ[j]
             F[j] = f(tmp_x)
         end
-        sort!(idx,by=i->F[i])
-        ∇f.=Z
-        ∇fσ.=Z
+        sort!(idx,by = i->F[i])
+        ∇f .= Z
+        ∇fσ .= Z
         for i in 1:samples
-            j=idx[i]
+            j = idx[i]
             ∇f .+= u[i] .* ϵ[j]
             ∇fσ .+= u[i] .* (ϵ[j] .* ϵ[j] .- O)
         end
         x .+= ημ .* σ .* ∇f
-        σ .*= exp.(ησ ./ 2 .*∇fσ)
+        σ .*= exp.(ησ ./ 2 .* ∇fσ)
     end
-    (sol=x, cost=f(x))
+    (sol = x, cost = f(x))
 end
 
 function separable_nes(f,μ::AbstractVector{T},σ::T,params::sNES{T}) where T
