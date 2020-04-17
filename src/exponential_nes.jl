cf61476565962842ddb4988ec911349dca0f056f
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-04-17 21:44:14 +0200

    inplace

--- src/exponential_nes.jl
+++ src/exponential_nes.jl
@@ -18,13 +18,15 @@ struct xNES{T}
     end
 end
 
+function δ(T,i,j)
+    ifelse(i==j,one(T),zero(T))
+end
 function exponential_nes(f,μ0::AbstractVector{T},A::AbstractMatrix{T},params::xNES{T}) where T
     n=params.samples
     ημ=params.ημ
     ησ=params.ησ
     ηB=params.ηB
     σtol=params.σtol
-
     μ=copy(μ0)
     d=length(μ)
     Z=Array{T}(undef,d,n)
@@ -33,20 +35,40 @@ function exponential_nes(f,μ0::AbstractVector{T},A::AbstractMatrix{T},params::x
     B=A./σ
     idx=collect(1:n)
     u=utility_function{T}(n)
-
+    Gδ=zeros(T,d)
+    GM=zeros(T,d,d)
     while σ>σtol
         randn!(Z)
         for i in 1:n
             F[i]=f(μ .+ σ .* B * Z[:,i])
         end
-        su=u[invperm(sort(idx,by=i->F[i]))]
-        Gδ=sum(su[i] .* Z[:,i] for i in 1:n)
-        GM=sum(su[i] .* (Z[:,i] * (Z[:,i]') - I) for i in 1:n)
+        sort!(idx,by=i->F[i])
+        #Gδ=sum(u[i] .* Z[:,idx[i]] for i in 1:n)
+        #GM=sum(u[i] .* (Z[:,idx[i]] * (Z[:,idx[i]]') - I) for i in 1:n)
+        Gδ.=0
+        GM.=0
+        for i in 1:n
+            j=idx[i]
+            for k in 1:d
+                Gδ[k]+=u[i] * Z[k,j]
+            end
+            for k1 in 1:d, k2 in 1:d
+                GM[k1,k2]+=u[i] * (Z[k1,j] * Z[k2,j] - δ(T,k1,k2))
+            end
+        end
         Gσ=tr(GM) / d
+        #=
         GB=GM - Gσ * I
         μ=μ .+ ημ * σ *B *Gδ
         σ=σ * exp(ησ/2 * Gσ)
-        B=B * exp(ηB/2 .* GB)
+        B=B * exp(ηB/2 .* GB)=#
+        Gσ=tr(GM) / d
+        for i in 1:d
+            GM[i,i]-=Gσ
+        end
+        μ=μ .+ ημ * σ *B *Gδ
+        σ=σ * exp(ησ/2 * Gσ)
+        B=B * exp(ηB/2 .* GM)
     end
     return (sol=μ, cost=f(μ))
 end
